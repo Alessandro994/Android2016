b37c2970151c247b701e3ea56abc97035fec9c35
Alessandro994 <alebacchiega51@gmail.com>
2016-03-04 09:42:56 +0100

    Now keyboard accept also decimal values. Result is now saved when orientation changes.

--- SimpleCalculator/app/src/main/java/com/example/alessandro/simplecalculator/MainActivity.java
+++ SimpleCalculator/app/src/main/java/com/example/alessandro/simplecalculator/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    static final String KEY_RESULT_VALUE = "result";
     static float RESULT = 0;
     static float A = 0;
     static float B = 0;
@@ -17,8 +18,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        if (savedInstanceState != null) {
+            Float savedResult = savedInstanceState.getFloat(KEY_RESULT_VALUE);
+            TextView textView = (TextView) findViewById(R.id.textView3);
+            textView.setText(String.valueOf(savedResult));
+        }
     }
 
+    @Override
+    protected void onSaveInstanceState (Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putFloat(KEY_RESULT_VALUE, RESULT);
+    }
 
     public void evaluateSum(View view) {
         EditText first = (EditText) findViewById(R.id.first_number);
@@ -51,7 +62,7 @@ public class MainActivity extends AppCompatActivity {
 
     public void showResult(View view) {
         TextView result = (TextView) findViewById(R.id.textView3);
-        result.setText(String.valueOf((Integer) Math.round(RESULT)));
+        result.setText(String.valueOf(RESULT));
     }
 
     public void clearFields(View view) {
